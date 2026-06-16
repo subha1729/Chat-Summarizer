ce09d8fc71406c0c75a3801584410b91550ed3ed
subha1729 <bera.subha2005@gmail.com>
2026-06-16 23:56:21 +0530

    changing

--- backend/services/aiService.js
+++ backend/services/aiService.js
@@ -4,6 +4,10 @@ const genAI = new GoogleGenerativeAI(
   process.env.GEMINI_API_KEY
 );
 
+console.log(
+  "Gemini Key Prefix:",
+  process.env.GEMINI_API_KEY?.substring(0, 10)
+);
 const model = genAI.getGenerativeModel({
   model: "gemini-2.5-flash-lite"
 });
